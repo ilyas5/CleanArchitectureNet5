23ffd71c9c8871bf89a39c02532125750eb44f6d
Muhammad Ilyas <47562000+ilyas5@users.noreply.github.com>
2021-06-16 18:58:18 +0500

    initial

---
+++ OnionApp/Persistence/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Application;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public static class DependencyInjection
+    {
+        public static void AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly("WebApi")));
+            services.AddScoped<IAppDbContext>(provider => provider.GetService<AppDbContext>());
+        }
+    }
+}
---
+++ OnionApp/WebApi/Controllers/StudentController.cs
@@ -0,0 +1,57 @@
+﻿using Application.CQRS.Commands;
+using Application.CQRS.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/student")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private IMediator mediator;
+        public StudentController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost, Route("create")]
+        public async Task<IActionResult> Create(CreateStudentCommand command)
+        {
+            return Ok(await mediator.Send(command));
+        }
+
+        [HttpGet, Route("getAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await mediator.Send(new GetAllStudentQuery()));
+        }
+       
+        [HttpGet, Route("getById")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(await mediator.Send(new GetStudentByIdQuery { Id = id }));
+        }
+
+        [HttpDelete, Route("delete")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok(await mediator.Send(new DeleteStudentByIdCommand { Id = id }));
+        }
+
+        [HttpPut, Route("update")]
+        public async Task<IActionResult> Update(int id, UpdateStudentCommand command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
+            return Ok(await mediator.Send(command));
+        }
+    }
+}
